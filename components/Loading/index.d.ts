8e0a44419e6c1d068c1e3d98321c3aaef8cbbe39
nicanororlando <nicanororlando@gmail.com>
2023-05-18 17:13:00 -0300

    Structure improved

--- components/Loading/index.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-interface ILoading {
-    show?: boolean;
-}
-export default function Loading({ show }: ILoading): React.JSX.Element;
-export {};
--- copyPackageJson.js
+++ copyPackageJson.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const sourcePath = path.join(__dirname, "package.json");
-const destPath = path.join(__dirname, "..", "package.json");
+const destPath = path.join(__dirname, "/build", "package.json");
 
 fs.copyFileSync(sourcePath, destPath);
 ``;
