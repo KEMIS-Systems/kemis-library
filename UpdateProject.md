234f89ad6167934cff5e74497ba22e23af973757
nicanororlando <nicanororlando@gmail.com>
2023-05-18 17:50:09 -0300

    Working

--- UpdateProject.md
+++ UpdateProject.md
@@ -1,8 +1,7 @@
 ### To Update/Publish this package
 
-1. Go to the \_development folder.
-2. Make the changes.
-3. Increase the version in the Package.json.
-4. Within the \_development folder, run `npm run build`.
-5. go to the root folder: `cd ..`.
-6. run `npm publish`.
+1. Make the changes.
+2. Increase the version in the package.json.
+3. run `npm run build`.
+4. go to the build folder: `cd build`.
+5. run `npm publish`.
--- src/hooks/Language/index.tsx
+++ src/hooks/Language/index.tsx
@@ -1,114 +1,23 @@
-import React, {
-  createContext,
-  useCallback,
-  useState,
-  useContext,
-  useMemo,
-  useEffect,
-} from 'react';
-import { useRouter } from 'next/router';
-import { ILanguage } from './languages';
-import api from '~/services/api';
+import { ILanguage } from "./languages";
 
 interface LanguageContextData {
   language: ILanguage;
-  idiom: string;
-  locale: string;
-  handleSelectIdiom(idiom: string): void;
 }
 
-export const LanguageContext = createContext<LanguageContextData>(
-  {} as LanguageContextData
-);
+export const Language = {} as LanguageContextData;
 
-interface IProps {
-  children: React.ReactNode;
-}
-
-export const LanguageProvider = ({ children }: IProps) => {
-  const router = useRouter();
-  const [idiom, setIdiom] = useState(() => {
-    let lang = router.locale;
-
-    if (typeof window !== 'undefined') {
-      if (lang !== 'en' && lang !== 'es') {
-        lang = localStorage.getItem('KEMISERP:language') || 'pt';
-      }
-      const element = document.getElementsByTagName('html');
-      element[0].lang = lang;
-    }
-
-    return lang || 'pt';
-  });
-  const [locale, setLocale] = useState('pt_BR');
-
-  useEffect(() => {
-    const idiomSelected = router.locale || 'pt_BR';
-    if (idiomSelected[0].length === 2) {
-      setIdiom(idiomSelected[0]);
-      let local = 'pt-BR';
-      if (idiom === 'en') {
-        local = 'en-US';
-      } else if (idiom === 'es') {
-        local = 'es-AR';
-      }
-      setLocale(local);
-    }
-  }, [router, idiom]);
-
-  const handleSelectIdiom = useCallback(
-    (idiomSelected: string) => {
-      setIdiom(idiomSelected);
-      const element = document.getElementsByTagName('html');
-      element[0].lang = idiomSelected;
-
-      let local = 'pt-BR';
-      if (idiom === 'en') {
-        local = 'en-US';
-      } else if (idiom === 'es') {
-        local = 'es-AR';
-      }
-      setLocale(local);
-      router.push(router.pathname, router.pathname, {
-        locale: idiomSelected,
-      });
-    },
-    [router, idiom]
-  );
-
-  const language = useMemo(() => {
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('KEMISERP:language', idiom);
-    }
+export const LanguageContextData = () => {
+  const language = () => {
+    let idiom = window.location.pathname.slice(0, 3);
+    if (idiom === null || idiom === undefined || idiom === "") idiom = "pt";
     return require(`./languages/${idiom}`);
-  }, [idiom]);
+  };
 
-  useEffect(() => {
-    api.defaults.headers.common['frontLanguage'] = idiom;
-  }, [idiom]);
-
-  const paramsProvider = useMemo(() => {
-    return {
-      language,
-      handleSelectIdiom,
-      idiom,
-      locale,
-    };
-  }, [language, handleSelectIdiom, idiom, locale]);
-
-  return (
-    <LanguageContext.Provider value={paramsProvider}>
-      {children}
-    </LanguageContext.Provider>
-  );
+  return language;
 };
 
 export function useLanguage(): LanguageContextData {
-  const context = useContext(LanguageContext);
+  const language = Language;
 
-  if (!context) {
-    throw new Error('useLanguage must be used within an LanguageProvider');
-  }
-
-  return context;
+  return language;
 }
