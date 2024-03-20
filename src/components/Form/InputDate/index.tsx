653c8015dbc112c07ee0201ebdfc66798b714098
Pedro Guilherme <pedro_racers@hotmail.com>
2024-03-20 15:27:51 -0300

    fix: Será melhorado a logica de exibição do file
    Neste commit foi melhorado o fluxo de renderização das imagens no
    componente ShowFile
    
    Perfomance
    
    Foi implemenado uma função para limpar o "lixo" gerado pela api URL na
    geração de links

--- src/components/Form/InputDate/index.tsx
+++ src/components/Form/InputDate/index.tsx
@@ -46,7 +46,7 @@ const InputDate = <T extends object>({
   hourFormat,
   selectionMode,
   readOnlyInput = false,
-}: IProps<T>) => {
+}: IProps<T>) => {  
   return (
     <div className={className ?? ""}>
       <Controller
@@ -65,7 +65,7 @@ const InputDate = <T extends object>({
                 {label}
               </label>
               <CalendarPrime
-                {...field}
+                {...field}                
                 id={field.name}
                 dateFormat={dateFormat ?? "dd/mm/yy"}
                 autoFocus={autoFocus}
@@ -82,10 +82,9 @@ const InputDate = <T extends object>({
                 className={
                   classNames({ "p-invalid ": fieldState.error }) + " w-full"
                 }
-                inputClassName="disabled:bg-slate-100"
-                disabled={disabled}
+                inputClassName={`disabled:bg-slate-100 ${fieldState.error ? 'p-invalid' : ''}`}
+                disabled={disabled}                
               />
-
               {<MessageError fieldState={fieldState} />}
             </>
           );
--- src/components/ShowFile/index.tsx
+++ src/components/ShowFile/index.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from "react";
 import { Toast } from "primereact/toast";
+import React, { useEffect, useRef, useState } from "react";
 
-import Loading from "../Loading";
 import { AxiosInstance } from "axios";
 import { generateUrlBlob, getFileNameOnRequest } from "../../utils/files";
+import Loading from "../Loading";
 
 interface P {
   [key: string]:
@@ -35,9 +35,9 @@ const ShowFile = ({
   onHide,
 }: IModalProps) => {
   const toast = useRef<Toast>(null);
-  const [showLoading, setShowLoading] = useState<boolean>(false);
-  const [imageUrl, setImageUrl] = useState<string>("");
-  const [pdfUrl, setPdfUrl] = useState<string>("");
+  const [showLoading, setShowLoading] = useState<boolean>(true);
+  const [isFile, setIsFile] = useState(false);
+  const [urlFile, setUrlFile] = useState<string | null>(null);
 
   useEffect(() => {
     setImageUrl("");
@@ -86,24 +86,77 @@ const ShowFile = ({
     }
   }, [url]);
 
+  function clearObjectUrl() {
+    try {
+      const pathObjectUrl = window.sessionStorage.getItem(
+        'mcf_admin@objectUrl'
+      );
+      if (!pathObjectUrl) return;
+      window.URL.revokeObjectURL(pathObjectUrl);
+    } catch (error) {
+      toast?.current?.show({
+        severity: "error",
+        summary: "Oops...",
+        detail: "Não foi possivel carregar o arquivo",
+      });
+      if (onHide) onHide();
+    }
+  }
+
+  useEffect(() => {
+    api
+      .get<Blob>(url, {
+        responseType: 'blob',        
+      })
+      .then((resolve) => {
+        if (resolve.status !== 200) {
+          setShowLoading(false);
+          return;
+        }
+
+        if (!window) {
+          setShowLoading(false);
+          return;
+        }
+
+        clearObjectUrl();
+
+        const urlCreated = window.URL.createObjectURL(resolve.data);
+
+        if (urlCreated) {
+          setUrlFile(urlCreated);
+          window.sessionStorage.setItem('mcf_admin@objectUrl', urlCreated);
+        }
+
+        if (resolve.headers['Content-Type']?.toString().includes('pdf')) {
+          setIsFile(true);
+        } else if (
+          resolve.headers['Content-Type']?.toString().includes('image')
+        ) {
+          setIsFile(false);
+        }
+
+        setShowLoading(false);
+      });
+  }, [url]);
+  
   return (
-    <>
-      {imageUrl && (
-        <div className="flex items-center justify-center min-w-full max-w-full min-h-full max-h-full">
-          <img
-            src={imageUrl}
-            alt={header}
-            className="w-full h-full object-scale-down"
-          />
-        </div>
-      )}
-      {pdfUrl && (
-        <iframe
-          src={pdfUrl}
-          title={header}
-          className="w-full min-h-screen max-h-screen"
+    <>      
+      <div data-showme={isFile} className="data-[showme=false]:flex hidden items-center justify-center min-w-full max-w-full min-h-full max-h-full">
+        <img
+          src={urlFile || ''}
+          alt={header}
+          className="w-full h-full object-scale-down"
         />
-      )}
+      </div>
+      
+      <iframe
+        data-showme={isFile}
+        src={urlFile || ''}
+        title={header}
+        className="data-[showme=true]:flex hidden w-full min-h-screen max-h-screen"
+      /> 
+
       <Toast ref={toast} />
       <Loading show={showLoading} />
     </>
