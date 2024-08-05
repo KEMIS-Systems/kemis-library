ff4d1c9b4c00de8488550ab4398cd5f6452e19ee
Pedro Guilherme <pedro_racers@hotmail.com>
2024-08-05 15:27:58 -0300

    fix:feat: Será adicionado um entry file funcional
    Neste commit foi adicionado um arquivo de entrada (entry file) para adequar
    o package como um Module funcional.
    
    @see https://docs.npmjs.com/about-packages-and-modules#about-packages

---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import {
+    AutoComplete,
+    CheckBox,
+    Chips,
+    ColorPalette,
+    DialogPhoto,
+    DialogSignature,
+    DrawSignature,
+    Dropdown,
+    Dropzone,
+    EditorHtml,
+    Form,
+    FormDialog,
+    InputDate,
+    InputFile,
+    InputImage,
+    InputMask,
+    InputNumber,
+    InputPassword,
+    InputSwitch,
+    InputText,
+    InputTextArea,
+    ListBox,
+    Loading,
+    MultiSelect,
+    RadioButton,
+    SelectButton,
+    ShowFile,
+    UploadPhoto,
+    UploadSignature,
+    WriteSignature,
+} from './components'
+import * as useLanguage from './hooks/Language'
+import * as useFormIntegration from './hooks/form'
+import * as MCnpj from './models/IMCnpj'
+import * as MIP from './models/IP'
+import * as Utils from './utils'
+
+export {
+    AutoComplete,
+    CheckBox,
+    Chips, ColorPalette, DialogPhoto, DialogSignature,
+    DrawSignature, Dropdown, Dropzone, EditorHtml,
+    Form,
+    FormDialog,
+    InputDate,
+    InputFile,
+    InputImage,
+    InputMask,
+    InputNumber,
+    InputPassword,
+    InputSwitch,
+    InputText,
+    InputTextArea,
+    ListBox, Loading, MCnpj,
+    MIP, MultiSelect,
+    RadioButton,
+    SelectButton, ShowFile, UploadPhoto, UploadSignature, useFormIntegration,
+    useLanguage, Utils, WriteSignature
+}
+
