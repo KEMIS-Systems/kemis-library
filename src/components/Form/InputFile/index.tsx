d0a8f933e43b5beb607b722d3ae093a3f5d5edc9
Jair Aparecido de Oliveira <jair.estu19@gmail.com>
2025-03-07 08:15:03 -0300

    Ajustes no componente de Autocomplete, DropDown, InputFile, InputNumber e InputText

--- src/components/Form/InputFile/index.tsx
+++ src/components/Form/InputFile/index.tsx
@@ -45,14 +45,19 @@ const InputFile = <T extends object>({
           render={({ field: { ref, onChange, ...field }, fieldState }) => {
             return (
               <>
-                <label htmlFor={field.name}>
+              <label
+               htmlFor={field.name}
+               className={
+               classNames({ "text-red-400 ": fieldState.error }) + " block"
+               }
+               >
                   {label}
                   {rules?.required ? (
                     <span className="text-slate-300"> *</span>
                   ) : (
-                    ""
+                  ""
                   )}
-                </label>
+              </label>
                 <Dropzone
                   accept={accept}
                   maxFiles={maxFiles}
