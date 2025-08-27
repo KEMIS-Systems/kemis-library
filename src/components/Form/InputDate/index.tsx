d360cae874dd34d39356bef3e0a0d87a778d8f73
Pedro Guilherme <pedro_racers@hotmail.com>
2025-08-27 10:55:12 -0300

    hotfix: Será melhorado a estrutura de InputDate
    Neste commit foi melhorado a estrutura do componente InputDate
    
    Changes
    
    - Foi resolvido o problema da captura de um valor pré-existente para o
    InputDate

--- src/components/Form/InputDate/index.tsx
+++ src/components/Form/InputDate/index.tsx
@@ -1,4 +1,4 @@
-import { addDays } from "date-fns";
+import { addDays, format } from "date-fns";
 import { classNames } from "primereact/utils";
 import React, { ChangeEvent, useCallback, useState } from "react";
 import {
@@ -74,15 +74,7 @@ const InputDate = <T extends object>({
           ""
         )}
       </label>
-      <input
-        type="date"
-        disabled={disabled}
-        className="appearance-none rounded-[6px] w-full h-[46px] p-[0.75rem] text-[#4b5563] bg-white border-[1px] transition-all duration-[0.2s] border-[#d1d5db] hover:border-[#4f46e5] focus:border-[#4f46e5] focus:shadow-md focus:shadow-[#a5f3fc] outline-none  disabled:bg-slate-100 disabled:hover:border-[#c2c2c2] data-[haserror=true]:border-[1.5px] data-[haserror=true]:border-red-400"
-        autoFocus={autoFocus}
-        inputMode="numeric"
-        readOnly={readOnlyInput}
-        onChange={(e) => handlerDateValue(e)}
-      />
+
       <Controller
         name={name}
         control={form?.control}
@@ -90,6 +82,18 @@ const InputDate = <T extends object>({
         render={({ field: { ...field }, fieldState, formState }) => {
           return (
             <>
+              <input
+                type="date"
+                disabled={disabled}
+                id={`input-date-${name}`}
+                name={`input-date-${name}`}
+                className="appearance-none rounded-[6px] w-full h-[46px] p-[0.75rem] text-[#4b5563] bg-white border-[1px] transition-all duration-[0.2s] border-[#d1d5db] hover:border-[#4f46e5] focus:border-[#4f46e5] focus:shadow-md focus:shadow-[#a5f3fc] outline-none  disabled:bg-slate-100 disabled:hover:border-[#c2c2c2] data-[haserror=true]:border-[1.5px] data-[haserror=true]:border-red-400"
+                autoFocus={autoFocus}
+                inputMode="numeric"
+                readOnly={readOnlyInput}
+                value={format(field.value || new Date(), "yyyy-MM-dd")}
+                onChange={(e) => handlerDateValue(e)}
+              />
               <input
                 type="text"
                 {...field}
