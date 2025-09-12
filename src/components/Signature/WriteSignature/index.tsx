6bfd00842768e92d6c60c54ef644ff199dee6183
Pedro Guilherme <pedro_racers@hotmail.com>
2025-09-12 11:05:04 -0300

    hotfix: Será atualizado para 2.13.61

--- src/components/Signature/WriteSignature/index.tsx
+++ src/components/Signature/WriteSignature/index.tsx
@@ -150,8 +150,8 @@ const WriteSignature = ({ onChange, text, writeSignature = true }: IProps) => {
         <div className="border border-gray-300 rounded-b-xl p-1">
           <div className="flex justify-between gap-2">
             {!showImage ? (
-              <div className="flex w-full gap-2 p-1 flex-row flex-nowrap items-center">
-                <div className="flex-1 max-w-[48%]">
+              <div className="kemis-library-signature-write-dropdown-container ">
+                <div className="kemis-library-signature-write-dropdown-item">
                   <Dropdown
                     name="font_type"
                     label={""}
@@ -161,7 +161,7 @@ const WriteSignature = ({ onChange, text, writeSignature = true }: IProps) => {
                     filter={false}
                   />
                 </div>
-                <div className="flex-1 max-w-[48%]">
+                <div className="kemis-library-signature-write-dropdown-item">
                   <Dropdown
                     name="font_size"
                     label={""}
